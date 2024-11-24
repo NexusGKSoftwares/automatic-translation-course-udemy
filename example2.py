4a5767a2459c867f771ba390e2f147214b9efd42
NexusGKSoftwares <nexusgk3@gmail.com>
2024-11-24 10:26:48 +0300

    "Changed translation language from Spanish to Arabic"

--- example2.py
+++ example2.py
@@ -1,6 +1,6 @@
 #transalated text to another language
 from translate import Translator
     #use googletrans library to translate text
-translator = Translator(from_lang="english", to_lang="spanish")
+translator = Translator(from_lang="english", to_lang="arabic")
 translation = translator.translate("this is a powerfull library")
 print(translation)
